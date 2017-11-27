0a0ca847db72387e6529e9edbe87694a61498f9a
Chuck Konkol <ckonkol@gmail.com>
2017-11-27 17:31:01 -0600

    ck

---
+++ MyGauges/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  MyGauges
+//
+//  Created by cis290 on 11/27/17.
+//  Copyright © 2017 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+@IBOutlet private weak var gaugeView: GaugeView!
+    
+@IBOutlet private weak var Gauge2: GaugeView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        gaugeView.percentage = 75
+        gaugeView.thickness = 5
+        gaugeView.labelFont = UIFont.systemFont(ofSize: 30, weight: UIFont.Weight.thin)
+        gaugeView.labelColor = UIColor.lightGray
+        gaugeView.labelText = "100"
+        gaugeView.gaugeBackgroundColor = UIColor.lightGray
+        gaugeView.gaugeColor = UIColor.blue
+        //gauge 2
+        Gauge2.percentage = 80
+        Gauge2.thickness = 5
+        Gauge2.labelFont = UIFont.systemFont(ofSize: 30, weight: UIFont.Weight.thin)
+        Gauge2.labelColor = UIColor.lightGray
+        Gauge2.labelText = "60"
+        Gauge2.gaugeBackgroundColor = UIColor.lightGray
+        Gauge2.gaugeColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
